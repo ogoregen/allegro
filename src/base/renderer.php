72c9a8eb5fa04c3022a965a63285249e5da8ba92
ogoregen <37447279+ogoregen@users.noreply.github.com>
2021-04-23 10:23:08 +0300

    Refactor render() to make $context optional

--- src/base/renderer.php
+++ src/base/renderer.php
@@ -1,7 +1,7 @@
 
 <?php
 
-function render($template, $context){
+function render($template, $context = []){
     //renders templates extending base template, passing variables
     include "templates/base.php";
 }
