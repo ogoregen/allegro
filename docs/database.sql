25c5bf01c8e3e57332ef0f05ef37b39550c722a7
ogoregen <37447279+ogoregen@users.noreply.github.com>
2021-05-01 00:37:56 +0300

    Update database.sql

--- docs/database.sql
+++ docs/database.sql
@@ -1,4 +1,9 @@
 
+CREATE DATABASE allegro;
+CREATE USER 'allegro'@'localhost' IDENTIFIED BY 'IDsvW.A!j*XajZ1t';
+GRANT ALL PRIVILEGES ON allegro.* TO 'allegro'@'localhost';
+FLUSH PRIVILEGES;
+
 CREATE TABLE User (
   id INT(11) AUTO_INCREMENT PRIMARY KEY,
   email VARCHAR(255) NOT_NULL,
