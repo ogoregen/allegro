83cef36f17fc704b07330b6bdf55e1b275d9e6e7
ogoregen <37447279+ogoregen@users.noreply.github.com>
2021-05-09 21:48:39 +0300

    Refactor url dispatching

--- src/public/index.php
+++ src/public/index.php
@@ -6,13 +6,12 @@
 session_set_cookie_params(2147483647); //maximum cookie lifespan
 session_start();
 
-require "../views.php";
 require "../urls.php";
+require "../views.php";
 
-if(isset($_GET["url"])) $url = explode("/", $_GET["url"]);
-else $url = [""];
-$path = $url[0];
+$uri = filter_var($_SERVER["REQUEST_URI"], FILTER_SANITIZE_URL);
 
-if(array_key_exists($path, $urls)) $view = $urls[$path]; //if url is valid
-else $view = $urls["404"];
+if(array_key_exists($uri, $urls)) $view = $urls[$uri]; //if url is valid
+else $view = $urls["/404"];
+
 $view();
