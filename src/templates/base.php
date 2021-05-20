5a46e1b01de99220bfa3a39d66e6e13a8f07738e
FatmaKr-06 <81985868+FatmaKr-06@users.noreply.github.com>
2021-05-20 19:41:08 +0300

    Revert "Create data/"
    This reverts commit f53e828fa367efacb861ce2d156ae6cb877b94c6.

--- src/templates/base.php
+++ src/templates/base.php
@@ -2,10 +2,10 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title><?= isset($context["title"]) ? $context["title"]." - " : "" ?>Allegro</title>
+        <title><?php echo $context["title"] ?></title>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="description" content="<?= $context["metaDescription"] ?? "" ?>">
+        <meta name="description" content="<?php echo $context["metaDescription"] ?? "" ?>">
         <link rel="stylesheet" href="static/css/style.css" type="text/css" charset="utf-8"/>
         <link rel="stylesheet" href="static/fonts/LeagueSpartanBold/stylesheet.css" type="text/css" charset="utf-8"/>
         <link rel="preconnect" href="https://fonts.gstatic.com">
