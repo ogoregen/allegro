3deeed061fe5fd287746fb3795c6591e3a978a74
FatmaKr-06 <81985868+FatmaKr-06@users.noreply.github.com>
2021-05-20 18:58:59 +0300

    Update urls.php

--- src/urls.php
+++ src/urls.php
@@ -6,7 +6,12 @@ $urls = [
     "/register" => "register",
     "/login" => "login",
     "/logout" => "logout",
+<<<<<<< Updated upstream
     "/404" => "_404",
     "/settings" => "settings",
     "/landing" => "landing",
+=======
+    "/404" => "_404,
+    "/settings" => "settings"
+>>>>>>> Stashed changes
 ];
