df8c4087c84f06119a4b594cc28b93b51c8fd576
ogoregen <37447279+ogoregen@users.noreply.github.com>
2021-06-01 20:55:20 +0300

    docs

--- src/core/view.php
+++ src/core/view.php
@@ -1,18 +1,38 @@
 <?php
 
+/**
+ * Utility functions for use in views.
+ */
+
 namespace Allegro\Core\view;
 
+/**
+ * Assert authentication, redirect to login if fails.
+ * 
+ * @param string $next GET variable to be passed to login view where it should be used as redirect location after login.
+ */
 function requireAuthentication($next = ""){
 	
-	if($next) $next = "?next=$next";
-	if(!isset($_SESSION["is_authenticated"])) header("Location: /login$next");
+	if(!isset($_SESSION["is_authenticated"])){
+
+		if($next) $next = "?next=$next";
+		header("Location: /login$next");
+	}
 }
 
-function requireUnauthentication(){
+/**
+ * Assert unauthentication, redirect to specified path or index if fails.
+ * 
+ * @param string $target path to redirect
+ */
+function requireUnauthentication($target = ""){
 
-	if(isset($_SESSION["is_authenticated"])) header("Location: /");
+	if(isset($_SESSION["is_authenticated"])) header("Location: /$target");
 }
 
+/**
+ * Assert POST request, redirect to index if fails. 
+ */
 function requirePOST(){
 
 	if($_SERVER["REQUEST_METHOD"] != "POST") header("Location: /");
