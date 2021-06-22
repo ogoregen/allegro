ea2f86c7167dfd1785bb676e9f0205109b156431
flighty <37447279+ogoregen@users.noreply.github.com>
2021-06-22 22:43:25 +0300

    Update and rename credentials.php to credentials.example.php

---
+++ src/config/credentials.example.php
@@ -0,0 +1,15 @@
+<?php
+
+//database
+define("DB_HOST", "localhost");
+define("DB_USER", "allegro");
+define("DB_PASS", "[PASSWORD]");
+define("DB_NAME", "allegro");
+
+//email
+define("MAIL_HOST", "smtp.example.com");
+define("MAIL_PORT", 465);
+define("MAIL_USER", "noreply@example.com");
+define("MAIL_PASS", "[PASSWORD]");
+define("MAIL_FROM_MAIL", "noreply@example.com");
+define("MAIL_FROM_NAME", "Allegro");
